f0a53907687a0695753cdf9f85041a294447b418
Alexander Latyshev <aladdelinxr@gmail.com>
2025-03-04 03:17:20 +0300

    add photo card

---
+++ src/components/photoCard/photoCard.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { motion } from "framer-motion";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
+interface PhotoCardProps {
+  imageUrl: string;
+  altText: string;
+  classname: string;
+}
+
+const PhotoCard: React.FC<PhotoCardProps> = ({
+  imageUrl,
+  altText,
+  classname,
+}) => {
+  return (
+    <motion.div
+      className="photo-card"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <LazyLoadImage
+        alt={altText}
+        src={imageUrl}
+        effect="blur"
+        width="100%"
+        height="auto"
+        className={`${classname}`}
+      />
+    </motion.div>
+  );
+};
+
+export default PhotoCard;
