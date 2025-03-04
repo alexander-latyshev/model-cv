554f0e6dfee454122769ded759945711fbf2b531
Alexander Latyshev <aladdelinxr@gmail.com>
2025-03-04 03:11:23 +0300

    Style: add styles sass

--- src/views/AboutMe.tsx
+++
@@ -1,9 +0,0 @@
-const AboutMe = () => {
-  return (
-    <section>
-      <h1>About me</h1>
-    </section>
-  );
-};
-
-export default AboutMe;
--- src/views/Portfolio.tsx
+++
@@ -1,5 +0,0 @@
-const Portfolio = () => {
-  return <section>Portfolio</section>;
-};
-
-export default Portfolio;
